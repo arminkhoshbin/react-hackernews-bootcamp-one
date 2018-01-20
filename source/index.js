aa5fbbbb32dea9902eca3edb7a79d64b91befdaf
Armin Khoshbin <armin.khoshbin@gmail.com>
2018-01-20 10:39:46 +1000

    completed chapter one

--- source/index.js
+++ source/index.js
@@ -3,14 +3,47 @@ import ReactDOM from 'react-dom';
 
 const title = 'Hello Hacker News';
 
-const element =
-  <div
-    style={{ fontSize: 36, color: 'green' }}
-    onClick={() => window.alert(title)}
-  >
-    {title}
-  </div>
+const _getStyles = function() {
+	return {
+		fontSize: 36, 
+		color: 'green'
+	}
+}
+
+function Element(props) {
+	return (
+		<div
+		  >
+    		{props.message}
+  		</div>
+	);
+}
+
+const element = <Element message = "Hello Hacker News" />
+
+const threeElements = 
+	<div>
+		<Element message = "Article 1" />
+		<Element message = "Article 2" />
+		<Element message = "Article 3" />
+	</div>
+
+function Article(props) {
+	return (
+		<div>
+			<h1>{props.title}</h1>
+			<span>{props.desc}</span>
+			<a href={props.href}>Link</a>
+		</div>
+	)
+}
+
+const article1 = 
+	<Article 
+		title="Fake Article" 
+		desc="This is a fake description" 
+		href="http://google.com" />
 
 const root = document.getElementById('root');
 
-ReactDOM.render(element, root);
+ReactDOM.render(article1, root);
