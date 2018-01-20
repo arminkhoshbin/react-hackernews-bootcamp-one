6f847c43a6be86641ea19a4cecfec9cee88ba7e7
Armin Khoshbin <armin.khoshbin@gmail.com>
2018-01-20 10:08:02 +1000

    completed first chapter

--- source/index.js
+++ source/index.js
@@ -1,6 +1,36 @@
-  document.body.innerHTML =
-    `
-      <div>
-        Hello Hacker News
-      </div>
-    `;
+  // document.body.innerHTML =
+  //   `
+  //     <div>
+  //       Hello Hacker News
+  //     </div>
+  //   `;
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const hello = 'Hello Hacker News';
+
+const _getStyles = function() {
+	return {
+		fontSize: 36,
+		color: 'green'
+	};
+};
+
+const onClickEvent = function() {
+	window.alert(hello);
+};
+
+const element = 
+	<div 
+		style={_getStyles()}
+		onClick={onClickEvent} >
+			{hello}
+	</div>
+;
+
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+);
